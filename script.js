ed426ab39839a72a162a07dfa8b3bd7fa21a23cf
Danielle Mcloughlin <184102558+daniellem62@users.noreply.github.com>
2025-01-03 19:16:20 +0000

    Completed ticket 4

--- script.js
+++ script.js
@@ -66,3 +66,43 @@ let productsArray = [
     console.log(addVAT);
 
    
+const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const doubleNum = numArray.map(numArray => numArray * 2);
+console.log(doubleNum);
+console.log(numArray);
+
+
+//- Initialize an array of strings
+const stringArray = ['Helloooooooooooo', 'World', 'I', 'am', 'a', 'string!'];
+
+//- Create a function that accepts a string parameter
+function stringLength(string) {
+    return string.length > 1;
+}
+
+//- It should return true if the string length is greater than 5   
+//- Use the filter method to filter out the strings with a length greater than 5
+const joined = stringArray.join(' ');
+const concat = stringArray.concat('Hello', 'World');
+const longString = stringArray.filter(stringLength);
+console.log(longString);
+console.log(joined);    
+console.log(concat);
+
+const slice = stringArray.slice(2, 4);
+console.log(slice);
+
+const index = stringArray.indexOf('World');
+console.log(index);
+
+const includes = stringArray.includes('World');
+console.log(includes);
+
+const reduceArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const reduced = reduceArray.filter((number) => number < 5).map(reduceArray => reduceArray * 10).reduce((prev, curr) => prev * curr);
+
+console.log(`The old array is ${reduceArray}`);
+console.log(`The new array is ${reduced}`);
+
